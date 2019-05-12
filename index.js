2bde5a3d848d4143d393af9ad48944d63942d349
WU <aa70608726@gmail.com>
2019-05-13 01:09:41 +0800

    update-2

--- index.js
+++ index.js
@@ -3,50 +3,77 @@ const ytdl = require('@microlink/youtube-dl');
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
+const ffmpeg = require('fluent-ffmpeg');
+const move = require('./middle/changefile')
+
 
 const app = express();
 
-app.use(express.static('./public'));
 
+app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/',(req, res)=>{
-  console.log(__dirname);
-  res.send('succ')
+
+app.post('/',(req,res) =>{
+  // const {url} = req.body;
+  let url = 'https://www.youtube.com/watch?v=KxgJdqfIk1s&list=RD9x2fSJxp2bg&index=10'
+  ytdl.exec(url, ['-x', '--audio-format', 'mp3'], {}, function(err, output) {
+    if (err) throw err;
+    console.log(output.join('\n'));
+  });
 })
+
 
 
 app.post('/download',(req, res)=>{
-  const { fileName } = req.body
-  const videoDownPath = path.resolve(__dirname, './public')
-  res.download(path.join(videoDownPath,info._filename),fileName);
+  const  {url} = req.body;
+  const { filename } = req.body
+  const videoDownPath = __dirname
+  if(url && filename){
+    return res.download(path.join(videoDownPath,filename));
+  } else {
+    return res.status(400).json({error: '錯誤'})
+  }
 })
 
 app.post('/ytdl',(req, res) =>{
+  const errors = {}
   const  {url} = req.body;
-  const videoDownPath = path.resolve(__dirname, './public')  
-  let  video = ytdl(url,
-    // Optional arguments passed to youtube-dl.
-    ['--format=18'],
-    // Additional options can be given for calling `child_process.execFile()`.  
-    { cwd: videoDownPath });
+  let accessList = ['https://www.youtube.com/','https://m.youtube.com/','https://youtu.be/']
+  if(url.indexOf('https://www.youtube.com/') === -1 &&
+  url.indexOf('https://m.youtube.com/') === -1 &&
+ url.indexOf('https://youtu.be/') === -1){
+    errors.input = 'this url invalid';
+    return res.status(400).json(errors)
+  }
+  let video = ytdl(url,
+  // Optional arguments passed to youtube-dl.
+  ['--format=18'],
+  // Additional options can be given for calling `child_process.execFile()`.
+  { cwd: __dirname });
   // Will be called when the download starts.
-    video.on('info', function(info) {
-      console.log('Download started');
-      console.log('filename: ' + info._filename);
-      console.log('size: ' + info.size);
-    let videodl = video.pipe(fs.createWriteStream(path.join(videoDownPath,info._filename)));
-    videodl.on('finish',(data)=>{
-      console.log('path.join(videoDownPath,info._filename)',path.join(videoDownPath,info._filename))      
-      res.download(path.join(videoDownPath,info._filename),info._filename);
-    })
-  })  
+  video.on('info', function(info) {
+    console.log('Download started');
+    console.log('filename: ' + info._filename);
+    ytdl.exec(url, ['-x', '--audio-format', 'mp3'], {}, function(err, output) {
+      if (err) throw err;
+      console.log(output)
+      console.log(output.join('\n'));
+      let oldName = info._filename
+      let newName = oldName.replace(/mp4/, 'mp3');
+      res.json({
+        filename: newName
+      })
+    });
+  });
+  
+  
 })
 
 
 
 
-app.listen(8000,()=>{
+app.listen(5311,()=>{
   console.log('port in 8000');
 })
